0860822b7a466a9888aa5e82988bcf11c1a6deae
unknown <singhpritishpratap@gmail.com>
2026-03-09 23:37:45 +0530

    Final Commit

---
+++ Task1.py
@@ -0,0 +1,18 @@
+
+print("\n")
+#Vairable input
+a = eval(input("Enter First Number: "))
+b = eval(input("Enter Second Number: "))
+
+
+#Addition
+print("Addition:",a+b)
+
+#subtraction
+print("Subtracton:",a-b)
+
+#Multiplication
+print("Multplicaton:",a*b)
+
+#Divition
+print("Divition:",a/b)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+n = str(input("Please enter your First name : "))
+m = str(input("Plese enter your Last name :"))
+k = n + m
+
+print(f"Hello,{n} {m}! Welcome to the Python program.")
